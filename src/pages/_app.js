a33c506b23c409e1ef896364d71205c769041b95
oldpilluwu <fawwazamin29@gmail.com>
2023-03-27 17:21:10 +0600

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import { NextUIProvider } from '@nextui-org/react';
+import {SessionProvider} from 'next-auth/react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, session }) {
+  return (
+    <SessionProvider session={session}>
+        <NextUIProvider>
+          <Component {...pageProps} />
+      </NextUIProvider>
+    </SessionProvider>
+  )
 }
