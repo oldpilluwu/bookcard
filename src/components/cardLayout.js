4aabadad05c4969a3d971da8e4997f90be83eaa0
Rafid <2018-025-318@student.cse.du.ac.bd>
2023-04-11 20:26:44 +0600

    new component added

--- src/components/cardLayout.js
+++ src/components/cardLayout.js
@@ -17,5 +17,41 @@ import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useRouter } from 'next/dist/client/router';
 import useUser from '@/lib/useUser'
+import { CardActionArea } from '@mui/material';
+import { Refresh } from '@mui/icons-material';
 
 
+
+
+const CardLayout = (props) => {
+        const {image , heading , description} = props
+    return ( 
+        <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+            <CardActionArea>
+            <CardMedia
+                component="img"
+                sx={{
+                // 16:9
+                pt: '56.25%',
+                }}
+                image={image}
+                alt="random" 
+            />
+            <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h5" component="h2">
+                {heading}
+                </Typography>
+                <Typography>
+                {description}
+                </Typography>
+            </CardContent>
+            {/* <CardActions>
+                <Button size="small">View</Button>
+                <Button size="small">Book</Button>
+            </CardActions> */}
+            </CardActionArea>
+        </Card>
+    )
+    }
+
+export default CardLayout
